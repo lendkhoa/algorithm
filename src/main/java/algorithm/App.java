0ca12b918515e071d45426ffc8daaf18e1d63b48
Khoa Le <khoale.inbox@gmail.com>
2021-12-25 15:55:01 -0600

    Add two sum problem

--- src/main/java/algorithm/App.java
+++ src/main/java/algorithm/App.java
@@ -4,10 +4,13 @@ package algorithm;
  * Hello world!
  *
  */
+
 public class App 
 {
     public static void main( String[] args )
     {
+        TwoSum agent = new TwoSum();
+        agent.solve(new int[]{}, 0);
         System.out.println( "Hello World!" );
     }
 }
---
+++ src/main/java/algorithm/TwoSum.java
@@ -0,0 +1,25 @@
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+// You can return the answer in any order.
+package algorithm;
+
+import java.util.HashMap;
+
+public class TwoSum {
+    public TwoSum() {}
+
+    public int[] solve(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap();
+
+        for (int i = 0; i < nums.length; i++) {
+            int newTarget = target - nums[i];
+            if(map.containsKey(newTarget)) {
+                return new int[]{i, map.get(newTarget)};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[]{};
+    }
+}
